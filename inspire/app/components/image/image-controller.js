f57173285987a4274dd4bf757551349f2a413502
Dakota Miller <DakotaJMiller123@gmail.com>
2016-07-14 23:58:40 -0600

    Changes to fix live break

--- inspire/app/components/image/image-controller.js
+++ inspire/app/components/image/image-controller.js
@@ -3,7 +3,7 @@
 	var imageService = new ImageService();
 	
 	imageService.getImage(function(res){
-		$('#body').css({ background: 'url(' + '//unsplash.it/g/1920/1080?random' + ')', color: 'black', 'background-size': 'cover'
+		$('#body').css({ background: 'url(' + 'https://unsplash.it/g/1920/1080?random' + ')', color: 'black', 'background-size': 'cover'
 	})
 	})
 	//Your ImageService is a global constructor function what can you do here if you new it up?
